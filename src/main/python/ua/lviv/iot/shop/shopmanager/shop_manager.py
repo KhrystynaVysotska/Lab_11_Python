b9ed3e46f4e1829a4cded5244dd6cfd0c9a90499
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-25 15:29:28 +0300

    Added method to sort by season

--- src/main/python/ua/lviv/iot/shop/shopmanager/shop_manager.py
+++ src/main/python/ua/lviv/iot/shop/shopmanager/shop_manager.py
@@ -1,6 +1,7 @@
 from typing import List
 from src.main.python.ua.lviv.iot.shop.model.abstract_goods_for_kids import AbstractGoodsForKids
 from src.main.python.ua.lviv.iot.shop.model.gender import Gender
+from src.main.python.ua.lviv.iot.shop.model.season import Season
 
 
 class ShopManager:
@@ -12,5 +13,12 @@ class ShopManager:
         list_of_founded_goods: List[AbstractGoodsForKids] = []
         for good in self.list_of_goods:
             if good.gender_category == gender:
+                list_of_founded_goods.append(good)
+        return list_of_founded_goods
+
+    def find_goods_by_season(self, season: Season) -> List[AbstractGoodsForKids]:
+        list_of_founded_goods: List[AbstractGoodsForKids] = []
+        for good in self.list_of_goods:
+            if good.season_name == season:
                 list_of_founded_goods.append(good)
         return list_of_founded_goods
